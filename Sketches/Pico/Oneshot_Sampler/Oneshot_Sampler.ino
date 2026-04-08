b38343365b6f5494fcbf5b5e65a920a39829c9bf
cob333 <147037012+cob333@users.noreply.github.com>
2026-04-08 13:46:05 +0800

    Modified description;

--- Sketches/Pico/Oneshot_Sampler/Oneshot_Sampler.ino
+++ Sketches/Pico/Oneshot_Sampler/Oneshot_Sampler.ino
@@ -1,7 +1,7 @@
 // Copyright 2026 Rich Heslip
 //
 // Author: Rich Heslip
-// Contributor: OpenAI Codex
+// Contributor: Wenhao Yang
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
